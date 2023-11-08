02bb1032c25e1e41821b38317ccf3951e3c774f3
Pierre-Henri Vong <pvong@c2r3s4.42lausanne.ch>
2023-11-08 15:31:28 +0100

    nothing much changed

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,23 +6,25 @@
 /*   By: pvong <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:28:26 by pvong             #+#    #+#             */
-/*   Updated: 2023/11/07 13:45:17 by pvong            ###   ########.fr       */
+/*   Updated: 2023/11/08 15:30:57 by pvong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
-int main(void) {
+int main( void ) {
     Fixed a;
-    Fixed const b(Fixed(5.05f) * Fixed(2));
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-    std::cout << "a: " << a << std::endl;
-    std::cout << "++a: " << ++a << std::endl;
-    std::cout << "a: " << a << std::endl;
-    std::cout << "a++: " << a++ << std::endl;
-    std::cout << "a: " << a << std::endl;
-    std::cout << "b: " << b << std::endl;
-    std::cout << "Fixed::max(a, b): " << Fixed::max(a, b) << std::endl;
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+
+    std::cout << b << std::endl;
+    
+    std::cout << Fixed::max( a, b ) << std::endl;
     return 0;
 }
